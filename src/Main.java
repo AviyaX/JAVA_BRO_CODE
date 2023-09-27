6e6dfde0c96c8d9ebc0f8152047360b6a8dd433c
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-27 10:49:56 +0530

    Expressions In Java =  Operands & Operators

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,21 @@ import java.util.Scanner; // including Scanner
 
 public class Main {
     public static void main(String[] args) {
-        //User Inputs In Java
-        Scanner scanner = new Scanner(System.in); // Creating Object
+        //Expressions In Java =  Operands & Operators
+            //Operands = values, Variables, numbers, quantity
+            // Operators - + - * / %
+        double friends =10;
 
-        System.out.println("What Is your Name: ");
-        String name = scanner.nextLine(); //nextLine will use for string
-        System.out.println("How Old Are You: ");
-        int age = scanner.nextInt(); //nextInt will use for int
-        scanner.nextLine(); //Clearing Scanner
-        System.out.println("what is Your Favourite Food");
-        String food =scanner.nextLine();
-
-
-        System.out.println("Hello My Name is "+name);
-        System.out.println("I'm "+age+" Years Old");
-        System.out.println("My favourite food is : "+food);
+//        friends= friends+1;   // Addition
+//        friends = friends -1; // Sub
+//        friends =friends *2;   //Multiply
+//        friends =friends / 2;    // Divide
+//        friends= friends %2; // Module - remaining after divide
+        friends = (double) friends / 3;    // onvert into different data type
 
 
 
-
+        System.out.println("you Have " +friends +" now");
 
     }
 }
