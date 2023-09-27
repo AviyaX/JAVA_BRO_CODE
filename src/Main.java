a0d7be80ff7b93087ef31690191c4a4c78d95240
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-27 16:36:49 +0530

    Random Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,13 @@
 import java.util.Scanner; // including Scanner
 import javax.swing.JOptionPane;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //Hypotenuse Of a triangle
-
-        double a;
-        double b;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter Side A : ");
-        a = scanner.nextDouble();
-        System.out.println("Enter Side B : ");
-        b = scanner.nextDouble();
-
-        double hyp = Math.sqrt((a*a) + (b*b));
-        System.out.println("Side C (hypotenus) :  " +hyp);
-
-        scanner.close();
-
-
-
-
-
-
-
-
-
-
-
+        //Random Values IN Java
+        Random random = new Random();
+        int x = random.nextInt(6)+1;
+        double y = random.nextDouble(6)+1;
+        System.out.println(y);
 
     }
 }
