8db1c67836df3cea2f325c909792b84cec359cd0
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-27 17:51:16 +0530

    If Statements

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,23 @@ import javax.swing.JOptionPane;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //Random Values IN Java
-        Random random = new Random();
-        int x = random.nextInt(6)+1;
-        double y = random.nextDouble(6)+1;
-        System.out.println(y);
+        //If Statements - Perform a block of code if it;s condition evaliate to be true
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Your Age : ");
+        int age = scanner.nextInt();
+        if(age>=75){
+            System.out.println("You Are OLD");
+
+        }
+        else if(age>=18) {
+            System.out.println("You are an Adult");
+
+        }
+        else{
+            System.out.println("Your Are NoT Adult ");
+        }
 
     }
 }
