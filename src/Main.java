488b220d49d6be153f227e7a690eb2558833af02
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-27 10:21:56 +0530

    Swap 2 Variables

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        //Variables
-        int x; //declaration
-        x=123; // Assignment
-        long y = 2132131267312L;
-        float z =3.14f;
-        double a=1.34;
-        boolean b=true;
-        String name ="Avishka Induwara";
+        //Swap 2 Variables
 
-        System.out.println("My Number Is : "+x);
-        System.out.println("This is a long number : "+y);
-        System.out.println("This is a float value "+z);
-        System.out.println("This is a double value: " +a);
-        System.out.println("This is a boolean Value: " +b);
-        System.out.println("This is my Name using Strig : "+name);
+        String x= "water";
+        String y="Cool-Aid";
+        String temp;
+        temp=x;
+        x=y;
+        y=temp;
+
+
+        System.out.println("X: "+x);
+        System.out.println("Y: "+y);
+
 
 
     }
