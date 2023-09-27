dfc2d1803a609d9a20dbf9f6a336c3007cca347e
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-27 10:03:25 +0530

    print, println, Escape Sequences

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //This is singe line comment in java
+        System.out.print("Hello and welcome!");
+        System.out.print("My Name Is Avishka Induwara"); //print prints in same line
+        System.out.println("I Love pizza "); // println prints on separate line
+        System.out.print("My name is Avishka\nInduwara");
+
+        /* This is multi line comment in java
+        Escape Sequences
+            \n - New Character (go to new line)
+            \t -  Empty Space (Tab)
+            "\\whatever\\" - \whatever\
+
+
+
+         */
+
+
+    }
+}
