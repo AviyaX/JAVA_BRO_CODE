e33caa526add88bc8c5c58865fd45eb9eb62a455
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-27 14:32:12 +0530

    Hypotenuse Of a triangle

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,27 @@ import java.util.Scanner; // including Scanner
 import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
-        //Mathematics Methods
-        double x = 3.14;
-        double y = 10;
+        //Hypotenuse Of a triangle
 
-        double z = Math.max(x,y);    //maximum
-        double a = Math.min(x,y);   // minimum
-        double abs = Math.abs(y);   //absolute
-        double sqrt = Math.sqrt(y);  //square root
-        double round = Math.round(x); // roundoff
-        double ceil = Math.ceil(x); //Rounded Up
-        double floor = Math.floor(x); //Rounded down
+        double a;
+        double b;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Side A : ");
+        a = scanner.nextDouble();
+        System.out.println("Enter Side B : ");
+        b = scanner.nextDouble();
+
+        double hyp = Math.sqrt((a*a) + (b*b));
+        System.out.println("Side C (hypotenus) :  " +hyp);
+
+        scanner.close();
 
 
 
 
-        System.out.println(z);
-        System.out.println(a);
-        System.out.println(abs); //absolute value no without -
-        System.out.println(sqrt);
-        System.out.println(round);
-        System.out.println(ceil);
-        System.out.println(floor);
+
 
 
 
