24d31f2a6d7598f2eee4d25238623f6366d780b8
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-27 17:59:09 +0530

    Switch cases

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,31 @@ import javax.swing.JOptionPane;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //If Statements - Perform a block of code if it;s condition evaliate to be true
+        // Switch cases
+           // - Statements that allows a variable to be tested for equality against a list of values
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter Your Age : ");
-        int age = scanner.nextInt();
-        if(age>=75){
-            System.out.println("You Are OLD");
-
-        }
-        else if(age>=18) {
-            System.out.println("You are an Adult");
-
-        }
-        else{
-            System.out.println("Your Are NoT Adult ");
+        String day  = "Monday";
+        switch(day){
+            case "Sunday":
+                System.out.println("It is Sunday");
+                break;
+            case "Monday":
+                System.out.println("It is Monday");
+                break;
+            case "Tuesday":
+                System.out.println("It is Tuesday");
+                break;
+            case "Wednesday":
+                System.out.println("It is Wednesday");
+                break;
+            case "Thursday":
+                System.out.println("It is Thursday");
+                break;
+            case "Friday":
+                System.out.println("It is Friday");
+                break;
+            default:
+                System.out.println("Thai Is Not A Date");
         }
 
     }
