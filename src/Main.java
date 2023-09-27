bd327be054211ea502ef1e8ed7a5d3141b85e2c9
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-27 10:33:59 +0530

    User Inputs In Java using Scanners

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
+import java.util.Scanner; // including Scanner
 
 public class Main {
     public static void main(String[] args) {
-        //Swap 2 Variables
+        //User Inputs In Java
+        Scanner scanner = new Scanner(System.in); // Creating Object
 
-        String x= "water";
-        String y="Cool-Aid";
-        String temp;
-        temp=x;
-        x=y;
-        y=temp;
+        System.out.println("What Is your Name: ");
+        String name = scanner.nextLine(); //nextLine will use for string
+        System.out.println("How Old Are You: ");
+        int age = scanner.nextInt(); //nextInt will use for int
+        scanner.nextLine(); //Clearing Scanner
+        System.out.println("what is Your Favourite Food");
+        String food =scanner.nextLine();
 
 
-        System.out.println("X: "+x);
-        System.out.println("Y: "+y);
+        System.out.println("Hello My Name is "+name);
+        System.out.println("I'm "+age+" Years Old");
+        System.out.println("My favourite food is : "+food);
+
+
 
 
 
