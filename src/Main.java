336f1279d5eed5900234ed09026dc6e07725f9b0
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-28 05:37:23 +0530

    Loops

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,47 @@ import javax.swing.JOptionPane;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        /* Logical Operators
-                   && -  (AND) - Both Conditions Must Be true
-                   || -  (OR)- At least one condition must be true
-                   ! -  (NOT) - Reverse Boolean Value of a condition
-         */
-//        int temp =15;
-//        if (temp>30){
-//            System.out.println("It is hot outside");
-//        }
-//        else if(temp>=20 && temp<=30){
-//            System.out.println("It is Warm Outside");
-//        }
-//        else{
-//            System.out.println("It is cold");
-//        }
+        // While Loop
+        //     - Executes a block of code as long as its Condition Remains true
 
 //        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Your Are playing a game! Press q or Q for exit.");
-//        String res = scanner.next();
-//        if(res.equals("q") || res.equals("Q")){ // checking Strings ==
-//            System.out.println("You Exit the game");
-//        }else{
-//            System.out.println("You are still playing the game");
+//        String name= "";
+//
+//       do{
+//            System.out.print("Enter Youer name  - ");
+//            name = scanner.nextLine();
+//
+//        } while(name.isBlank());
+//        System.out.println("Hello "+name);
+
+        // For Loop IN java - Execute a block of code a limited amount of time
+//
+//         for(int index=10; index>=0; index-=2){
+//             System.out.println(index);
+//         }
+//        System.out.println("Happy New Year");
+
+        // Nested Loops  - A loop inside a loop
+
+//        Scanner scanner = new Scanner(System.in);
+//        int row;
+//        int columns;
+//        String symbol = "";
+//        System.out.println("Enter No of rows : ");
+//        row=scanner.nextInt();
+//        System.out.println("Enter No of Columns: ");
+//        columns= scanner.nextInt();
+//        System.out.println("Enter Symbol to use : ");
+//        symbol=scanner.next();
+//
+//        for (int i=1;i<=row;i++ ){
+//            System.out.println();
+//            for(int j=1;j<=columns;j++){
+//                System.out.print(symbol);
+//            }
 //        }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Your Are playing a game! Press q or Q for exit.");
-        String res = scanner.next();
-        if(!res.equals("q") && !res.equals("Q")){ // checking Strings ==
-            System.out.println("You are still playing the game");
-        }else{
-            System.out.println("You Exit the game");
-        }
+
+
     }
 }
