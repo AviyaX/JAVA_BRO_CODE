f46ad11835453c631223035c82108ed7cb84610f
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-28 05:51:17 +0530

    Array

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,20 @@ import javax.swing.JOptionPane;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        // While Loop
-        //     - Executes a block of code as long as its Condition Remains true
+       // Array - Array is used to store multiple values in a single variable
+        String [] cars ={"BMW", "Toyota", "Mustang", "tesla"};
+        cars[0] = "Camaro";
+        System.out.println(cars[0]);
 
-//        Scanner scanner = new Scanner(System.in);
-//        String name= "";
-//
-//       do{
-//            System.out.print("Enter Youer name  - ");
-//            name = scanner.nextLine();
-//
-//        } while(name.isBlank());
-//        System.out.println("Hello "+name);
+        String[] cars =new String[3];
+        cars[0] = "Camaro";
+        cars[1] = "BMW";
+        cars[2] = "Tesla";
+        System.out.println(cars[0]);
 
-        // For Loop IN java - Execute a block of code a limited amount of time
-//
-//         for(int index=10; index>=0; index-=2){
-//             System.out.println(index);
-//         }
-//        System.out.println("Happy New Year");
-
-        // Nested Loops  - A loop inside a loop
-
-//        Scanner scanner = new Scanner(System.in);
-//        int row;
-//        int columns;
-//        String symbol = "";
-//        System.out.println("Enter No of rows : ");
-//        row=scanner.nextInt();
-//        System.out.println("Enter No of Columns: ");
-//        columns= scanner.nextInt();
-//        System.out.println("Enter Symbol to use : ");
-//        symbol=scanner.next();
-//
-//        for (int i=1;i<=row;i++ ){
-//            System.out.println();
-//            for(int j=1;j<=columns;j++){
-//                System.out.print(symbol);
-//            }
-//        }
+        for(int i=0; i< cars.length; i++){
+            System.out.println(cars[i]);
+        }
 
 
 
