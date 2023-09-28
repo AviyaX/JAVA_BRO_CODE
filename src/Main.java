02d26bbc049bf849e1689f7a59a84df7bab2d579
Avishka Induwara <v.g.a.induwara@gmail.com>
2023-09-28 06:36:41 +0530

    2D Array

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,28 @@ import javax.swing.JOptionPane;
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-       // Array - Array is used to store multiple values in a single variable
-        String [] cars ={"BMW", "Toyota", "Mustang", "tesla"};
-        cars[0] = "Camaro";
-        System.out.println(cars[0]);
+      ///2D Array  -  an array of arrays
 
-        String[] cars =new String[3];
-        cars[0] = "Camaro";
-        cars[1] = "BMW";
-        cars[2] = "Tesla";
-        System.out.println(cars[0]);
+      String [][] cars =new String[3][3];
+      cars[0][0] = "Camaro";
+      cars[0][1] = "Corvette";
+      cars[0][2] = "Silvarado";
 
-        for(int i=0; i< cars.length; i++){
-            System.out.println(cars[i]);
-        }
+      cars[1][0] = "Ranger";
+      cars[1][1] = "mustang";
+      cars[1][2] = "Camaro";
+
+      cars[2][0] = "volvo";
+      cars[2][1] = "tesla";
+      cars[2][2] = "BMW";
+
+      for(int i=0; i<cars.length;i++){
+          System.out.println();
+          for (int j=0; j<cars[1].length; j++){
+              System.out.print(cars[i][j]+" ");
+          }
+      }
+
 
 
 
